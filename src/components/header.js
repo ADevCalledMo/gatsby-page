d0e9993e613c78045ec98c3d2b8f6ee7ff854f4c
WalkerMozes <mozeswalker@outlook.com>
2021-01-17 11:11:18 +0000

    started styling gatsby page with scss modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 import React from "react"; 
 import { Link } from "gatsby"
 
+import headerStyles from "./header.module.scss"
+
 const Header = () => {
     return (
         <header>
             <h1>Mozes Walker</h1>
             <ul>
-                <li><Link to="/">Home</Link></li>
+                <li><Link className={headerStyles.link} to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/blog">Blog</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
