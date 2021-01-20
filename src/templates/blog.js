addd04f0aaf58e2f52a77e2c855e3bab9b6a61f5
WalkerMozes <mozeswalker@outlook.com>
2021-01-20 11:56:00 +0000

    implemented rendering post data into blog template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,13 +1,28 @@
-import React from "react"; 
+import React from "react";
+import { graphql } from "gatsby";
 
-import Layout from "../components/layout"
- 
-const Blog = () => {
-    return (
-        <Layout>
-            This is a Blog template! 
-        </Layout>
-    )
-}
+import Layout from "../components/layout";
 
-export default Blog; 
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`;
+
+const Blog = (props) => {
+  return (
+  <Layout>
+        <h1> {props.data.markdownRemark.frontmatter.title}</h1>
+        <p> {props.data.markdownRemark.frontmatter.date}</p>
+        <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+  </Layout>
+  )
+};
+
+export default Blog;
