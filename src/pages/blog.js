a5a6929a4f5dee51e422666c159ff6f04a2ee855
WalkerMozes <mozeswalker@outlook.com>
2021-01-20 11:07:05 +0000

    added templates to future blog posts also added links to blog posts on blog page that directs to template

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,36 +1,41 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
+import { Link, graphql, useStaticQuery } from "gatsby";
 
 import Layout from "../components/layout";
 
 const BlogPage = () => {
-    const data = useStaticQuery(graphql`
-        query{
-            allMarkdownRemark {
-                edges{
-                    node{
-                        frontmatter{
-                            title
-                            date
-                        }
-                    }
-                }
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
             }
+            fields {
+              slug
+            }
+          }
         }
-    `)
+      }
+    }
+  `);
 
   return (
     <Layout>
       <h1>Blog Posts</h1>
       <ol>
-          {data.allMarkdownRemark.edges.map((edges) => {
-            return (
-                <li>
-                    <h2>{edges.node.frontmatter.title}</h2>
-                    <p>{edges.node.frontmatter.date}</p>
-                </li>
-            )
-          })}
+        {data.allMarkdownRemark.edges.map(edges => {
+          return (
+            <li>
+              <Link to={`/blog/${edges.node.fields.slug}`}>
+                <h2>{edges.node.frontmatter.title}</h2>
+                <p>{edges.node.frontmatter.date}</p>
+              </Link>
+            </li>
+          );
+        })}
       </ol>
     </Layout>
   );
