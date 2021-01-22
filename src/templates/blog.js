5826c58653811db17cd3fe294336fba64ae565bd
WalkerMozes <mozeswalker@outlook.com>
2021-01-22 17:39:28 +0000

    integrated contentful fully with my blog, images load

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,28 +1,63 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { renderRichText } from "gatsby-source-contentful/rich-text";
+import { BLOCKS } from "@contentful/rich-text-types";
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout";
+// query($slug: String!) {
+//   contentfulBlogPost(slug: { eq: $slug }) {
+//     title
+//     date(formatString: "Do MMMM YYYY")
+//     body {
+//       raw
+//     }
+//   }
+// }
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      date(formatString: "Do MMMM YYYY")
+      body {
+        raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            __typename
+            title
+            fixed(width: 750) {
+              src
+            }
+          }
+        }
       }
-      html
     }
   }
 `;
 
-const Blog = (props) => {
+const Blog = props => {
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        const {
+          fixed: { src },
+          title
+        } = node.data.target;
+
+        return <img src={src} alt={title} />;
+      }
+    }
+  };
+
   return (
-  <Layout>
-        <h1> {props.data.markdownRemark.frontmatter.title}</h1>
-        <p> {props.data.markdownRemark.frontmatter.date}</p>
-        <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
-  </Layout>
-  )
+    <Layout>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.date}</p>
+      {renderRichText(props.data.contentfulBlogPost.body, options)}
+    </Layout>
+  );
 };
 
 export default Blog;
